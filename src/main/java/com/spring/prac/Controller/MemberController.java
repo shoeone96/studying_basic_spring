4f67a041c2e87bfb37797eb3c86d398148f6dd19
shoeone96 <dlwnddnjs96@gmail.com>
2021-08-12 00:15:34 +0900

    jdbc를 이용한 repository db 저장 및 CRUD

--- src/main/java/com/spring/prac/Controller/MemberController.java
+++ src/main/java/com/spring/prac/Controller/MemberController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.security.SecureRandom;
+
 @Controller
 public class MemberController {
 
@@ -30,4 +32,5 @@ public class MemberController {
         memberService.join(member);
         return "redirect:/";
     }
+
 }
